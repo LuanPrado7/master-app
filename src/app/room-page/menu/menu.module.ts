056132798fb01391445d2447e5c93379eb1d4d22
Leilah Nogueira <leilah_abn@hotmail.com>
2018-11-26 17:45:09 -0200

    importanto notifiermodule para menu

--- src/app/room-page/menu/menu.module.ts
+++ src/app/room-page/menu/menu.module.ts
@@ -5,12 +5,53 @@ import { MenuProfileComponent } from "./menu-profile/menu-profile.component";
 import { MenuStoreComponent } from "./menu-store/menu-store.component";
 import { MenuRankingComponent } from './menu-ranking/menu-ranking.component';
 import { MatDialogModule, MatTableModule, MatIconModule } from '@angular/material';
+import { NotifierModule, NotifierOptions } from 'angular-notifier';
+
+const notifierOptions: NotifierOptions = {
+  position: {
+    horizontal: {
+      position: "right"
+    },
+    vertical: {
+      position: "top"
+    }
+  },
+  theme: "material",
+  behaviour: {
+    autoHide: 5000,
+    onClick: "hide",
+    onMouseover: "pauseAutoHide",
+    showDismissButton: false
+  },
+  animations: {
+    enabled: true,
+    show: {
+      preset: "slide",
+      speed: 300,
+      easing: "ease"
+    },
+    hide: {
+      preset: "fade",
+      speed: 300,
+      easing: "ease",
+      offset: 50
+    },
+    shift: {
+      speed: 300,
+      easing: "ease"
+    },
+    overlap: 150
+  }
+};
 
 @NgModule({
   imports: [CommonModule, NgxSpinnerModule,
     MatDialogModule,
     MatTableModule,
-    MatIconModule],
+    MatIconModule,
+    NotifierModule,
+    NotifierModule.withConfig(notifierOptions)
+  ],
   exports: [MenuStoreComponent, MenuProfileComponent, MenuRankingComponent],
   declarations: [
     MenuStoreComponent, 
